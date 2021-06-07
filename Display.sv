edbdd1235479c65da257119c27c3a0c2267649f9
slienhmjgvgerghrgewahrfvvnmjeige <77601091+slienhmjgvgerghrgewahrfvvnmjeige@users.noreply.github.com>
2021-06-07 01:46:03 -0700

    added lines

--- Display.sv
+++ Display.sv
@@ -19,69 +19,69 @@ module Display(
     always_ff @(posedge clk) begin
         // Square 1
         if ((x > 0 & x < 213 & y > 0 & y < 160) & gold[0])
-            {r, g, b} <= ~24'hB8860B; // gold
+            {r, g, b} <= 24'hB8860B; // gold
         else if((x > 0 & x < 213 & y > 0 & y < 160) & purp[0])
-            {r, g, b} <= ~24'h800080;// purple
+            {r, g, b} <= 24'h800080;// purple
         else if((x > 0 & x < 213 & y > 0 & y < 160) & !gold[1] & !purp[1])
-            {r, g, b} <= 24'h000000;// default black
+            {r, g, b} <= 24'hFFFFFFF;// default black
         // Square 2
-        else if((x > 213 & x < 426 & y > 0 & y < 160) & gold[1])
+        else if((x > 215 & x < 426 & y > 0 & y < 160) & gold[1])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & purp[1])
+        else if((x >= 215 & x < 426 & y > 0 & y < 160) & purp[1])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & !gold[1] & !purp[1])
+        else if((x >= 215 & x < 426 & y > 0 & y < 160) & !gold[1] & !purp[1])
             {r, g, b} <= 24'hFFFFFF;// default black
         // Square 3
-        else if((x > 426 & x < 640 & y > 0 & y < 160) & gold[2])
+        else if((x > 428 & x < 640 & y > 0 & y < 160) & gold[2])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x > 426 & x < 640 & y > 0 & y < 160) & purp[2])
+        else if((x > 428 & x < 640 & y > 0 & y < 160) & purp[2])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & !gold[2] & !purp[2])
+        else if((x >= 428 & x < 640 & y > 0 & y < 160) & !gold[2] & !purp[2])
             {r, g, b} <= 24'hFFFFFF;// default black
         // Square 4
-        else if((x > 0  & x < 213 & y > 160 & y < 320) & gold[3])
+        else if((x > 0  & x < 213 & y > 162 & y < 320) & gold[3])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x > 0  & x < 213 & y > 160 & y < 320) & purp[3])
+        else if((x > 0  & x < 213 & y > 162 & y < 320) & purp[3])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & !gold[3] & !purp[3])
+        else if((x >= 0 & x < 213 & y > 162 & y < 320) & !gold[3] & !purp[3])
             {r, g, b} <= 24'hFFFFFF;// default black
         // Square 5
-        else if((x > 213 & x < 426 & y > 160 & y < 320) & gold[4])
+        else if((x > 215 & x < 426 & y > 162 & y < 320) & gold[4])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x > 213 & x < 426 & y > 160 & y < 320) & purp[4])
+        else if((x > 215 & x < 426 & y > 162 & y < 320) & purp[4])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & !gold[4] & !purp[4])
+        else if((x >= 215 & x < 426 & y > 162 & y < 320) & !gold[4] & !purp[4])
             {r, g, b} <= 24'hFFFFFF;// default black
         // Square 6
-        else if((x > 426 & x < 640 & y > 160 & y < 320) & gold[5])
+        else if((x > 428 & x < 640 & y > 162 & y < 320) & gold[5])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x > 426 & x < 640 & y > 160 & y < 320) & purp[5])
+        else if((x > 428 & x < 640 & y > 162 & y < 320) & purp[5])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & !gold[5] & !purp[5])
+        else if((x >= 428 & x < 426 & y > 162 & y < 320) & !gold[5] & !purp[5])
             {r, g, b} <= 24'hFFFFFF;// default black
         // Square 7
-        else if((x > 0 & x < 213 & y > 320 & y < 480) & gold[6])
+        else if((x > 0 & x < 213 & y > 322 & y < 480) & gold[6])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x > 0 & x < 213 & y > 320 & y < 480) & purp[6])
+        else if((x > 0 & x < 213 & y > 322 & y < 480) & purp[6])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & !gold[6] & !purp[6])
+        else if((x >= 213 & x < 426 & y > 322 & y < 480) & !gold[6] & !purp[6])
             {r, g, b} <= 24'hFFFFFF;// default black
         // Square 8
-        else if((x > 213 & x < 426 & y > 320 & y < 480) & gold[7])
+        else if((x > 215 & x < 426 & y > 322 & y < 480) & gold[7])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x > 213 & x < 426 & y > 320 & y < 480) & purp[7])
+        else if((x > 215 & x < 426 & y > 322 & y < 480) & purp[7])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x >= 213 & x < 426 & y > 0 & y < 160) & !gold[7] & !purp[7])
+        else if((x >= 215 & x < 426 & y > 322 & y < 480) & !gold[7] & !purp[7])
             {r, g, b} <= 24'hFFFFFF;// default black
         // Square 9
-        else if((x > 426 & x < 640 & y > 320 & y < 480) & gold[8])
+        else if((x > 428 & x < 640 & y > 322 & y < 480) & gold[8])
             {r, g, b} <= 24'hF9C700; // gold
-        else if((x > 426 & x < 640 & y > 320 & y < 480) & purp[8])
+        else if((x > 428 & x < 640 & y > 322 & y < 480) & purp[8])
             {r, g, b} <= 24'h6600CC;// purple
-        else if((x > 426 & x < 640 & y > 320 & y < 480) & !gold[8] & !purp[8])
+        else if((x > 428 & x < 640 & y > 322 & y < 480) & !gold[8] & !purp[8])
             {r, g, b} <= 24'hFFFFFF;// default black    
         else
-            {r, g, b} <= 24'hFFFFFF;// default black 
+            {r, g, b} <= 24'h000000;// default black 
     end
 endmodule
 
