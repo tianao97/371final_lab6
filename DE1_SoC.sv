fc79726ac99e8070dbd5fb15dc8be732ac93caac
slienhmjgvgerghrgewahrfvvnmjeige <77601091+slienhmjgvgerghrgewahrfvvnmjeige@users.noreply.github.com>
2021-06-06 23:00:46 -0700

    added intermediat assigns

--- DE1_SoC.sv
+++ DE1_SoC.sv
@@ -19,7 +19,7 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW,
     output logic N8_PULSE;
 
 	logic reset, game_reset;
-    logic up, down, select;
+    logic up, down, select, up1, down1, select1, start1;
     logic game_finished, purple_win; 
     logic [3:0] count;
     logic [8:0] square, purp_state, gold_state;
@@ -30,8 +30,13 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW,
     //Inputs
     assign reset = SW[0];
 
-    n8_driver driver( .clk(CLOCK_50), .data_in(N8_DATA_IN), .latch(N8_LATCH), .pulse(N8_PULSE), .up(up),
-                      .down(down), .select(select), .start(game_reset));
+    n8_driver driver( .clk(CLOCK_50), .data_in(N8_DATA_IN), .latch(N8_LATCH), .pulse(N8_PULSE), .up(up1),
+                      .down(down1), .select(select1), .start(start1));
+    //Assign intermediate logic (as suggested by padlet)
+    assign up = up1;
+    assign down = down1;
+    assign slect = select1;
+    assign game_reset = start1;
 
     n8_to_key n8_decoder (.clk(CLOCK_50), .reset(reset), .up(up), .down(down), .select(select), .count(count), .square(square));
 
@@ -58,8 +63,6 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW,
     assign LEDR[8] = down;
     assign LEDR[7] = select;
     assign LEDR[6] = game_reset;
-
-
 
     assign HEX0 = '1;
 	assign HEX1 = '1;
